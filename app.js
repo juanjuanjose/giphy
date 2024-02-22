7d496c3aff0b121e21fdaca6aa1fd995a4fd6bd6
juan jose <juanjoperez2121@gmail.com>
2024-02-22 09:38:40 -0500

    Se sube el trabajo

---
+++ app.js
@@ -0,0 +1,33 @@
+
+function cargarTendencias() {
+    const apiKey = 'TU_CLAVE_DE_API_DE_GIPHY';
+  
+  
+    const apiUrl = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=10`;
+  
+
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        const gifs = data.data;
+  
+   
+        const container = document.getElementById('tendencias-container');
+        container.innerHTML = '';
+  
+        gifs.forEach(gif => {
+          const gifUrl = gif.images.fixed_height.url;
+          const imgElement = document.createElement('img');
+          imgElement.src = gifUrl;
+          imgElement.alt = gif.title;
+          container.appendChild(imgElement);
+        });
+      })
+      .catch(error => {
+        console.error('Error al cargar tendencias:', error);
+      });
+  }
+  
+  const cargarTendenciasBtn = document.getElementById('cargarTendenciasBtn');
+  cargarTendenciasBtn.addEventListener('click', cargarTendencias);
+  
